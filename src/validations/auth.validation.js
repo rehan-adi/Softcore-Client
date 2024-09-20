c64bfd2b50836d3bcfb14725af1d5ea3c9768805
Rehan <rehanalire52@gmail.com>
2024-09-20 20:56:33 +0530

    Add zod validation

---
+++ src/validations/auth.validation.js
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const SignupValidation = z.object({
+  username: z.string()
+    .min(2, "Username must be at least 2 characters")
+    .max(10, "Username must be at most 10 characters"),
+  fullname: z.string()
+    .min(2, "Full Name must be at least 2 characters")
+    .max(20, "Full Name must be at most 20 characters"),
+  email: z.string()
+    .email("Invalid email address")
+    .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid email format"),
+  password: z.string()
+    .min(1, "Password must be needed")
+    .max(6, "Password must be at most 6 characters"),
+});
