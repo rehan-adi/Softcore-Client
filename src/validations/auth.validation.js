e77a69e457588fb67082553b4d7522bda727029d
Rehan <rehanalire52@gmail.com>
2024-09-22 11:55:33 +0530

    Update auth validation

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -1,25 +1,27 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const SignupValidation = z.object({
   username: z.string()
-    .min(2, "Username must be at least 2 characters")
-    .max(10, "Username must be at most 10 characters"),
+    .min(2, 'Username must be at least 2 characters')
+    .max(20, 'Username must be at most 20 characters'),
   fullname: z.string()
-    .min(2, "Full Name must be at least 2 characters")
-    .max(20, "Full Name must be at most 20 characters"),
+    .min(2, 'Full Name must be at least 2 characters')
+    .max(20, 'Full Name must be at most 20 characters'),
   email: z.string()
-    .email("Invalid email address")
-    .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid email format"),
+    .email('Invalid email address')
+    .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Invalid email format'),
   password: z.string()
-    .min(6, "Password must be at least 6 characters")
-    .max(15, "Password must be at most 15 characters"),
+    .min(6, 'Password must be at least 6 characters')
+    .max(15, 'Password must be at most 15 characters'),
+  profilePicture: z.string().optional(),
+  bio: z.string().optional()
 });
 
 export const SigninValidation = z.object({
   email: z.string()
-    .email("Invalid email address")
-    .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid email format"),
+    .email('Invalid email address')
+    .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Invalid email format'),
   password: z.string()
-    .min(6, "Password must be at least 6 characters")
-    .max(15, "Password must be at most 15 characters"),
+    .min(6, 'Password must be at least 6 characters')
+    .max(15, 'Password must be at most 15 characters'),
 });
