b5071f20ca809f9ec34f56d0e2b286bb06da61f2
Rehan <rehanalire52@gmail.com>
2024-09-23 11:46:07 +0530

    add logo link to navbar for homepage navigation

--- src/components/SideNavbar.jsx
+++ src/components/SideNavbar.jsx
@@ -37,29 +37,30 @@ function SideNavbar() {
   return (
     <div className="min-h-screen z-50 fixed bg-black md:flex md:flex-col hidden border-r border-white border-opacity-20 w-[250px]">
       <nav className="w-full flex border-b border-white border-opacity-20 py-4 justify-center items-center">
-        <svg
-          width="50"
-          height="50"
-          className="text-white"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 187.47 94.63"
-        >
-          <g fill="currentColor">
-            <polygon points="100.99 45.87 120.42 35.22 120.42 8.22 27.09 59.79 27.09 86.41 71.84 61.56 71.84 46.99 87.44 38.3 100.99 45.87"></polygon>
-            <polygon points="0 87.63 12.61 94.54 24.29 88 24.29 36.9 0 23.26 0 87.63"></polygon>
-            <polygon points="74.64 48.67 74.64 61.47 135.18 94.63 147.41 87.81 147.41 75.3 88.09 41.2 74.64 48.67"></polygon>
-            <polygon points="70.91 32.42 12.05 0 0 6.63 0 20.18 46.43 45.96 70.91 32.42"></polygon>
-            <polygon points="123.22 6.73 123.22 58.39 147.41 71.84 147.41 7.1 134.99 0.19 123.22 6.73"></polygon>
-            <path d="M177.78,14.27c0-3.41-2.27-5.9-6.61-5.9h-7V25.53h4.21V19.81H171l2.88,5.72h4.69L174.85,19A5.14,5.14,0,0,0,177.78,14.27Zm-6.61,2.06h-2.84v-4h2.84c1.49,0,2.36.7,2.36,1.93C173.53,15.79,172.39,16.33,171.17,16.33Z"></path>
-            <path d="M187.47,17.17a17,17,0,0,0-17-17h0a17,17,0,1,0,17,17Zm-16.9,14.09h-.12a14,14,0,1,1,.12,0Z"></path>
-          </g>
-        </svg>
+        <Link to="/">
+          <svg
+            width="50"
+            height="50"
+            className="text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 187.47 94.63"
+          >
+            <g fill="currentColor">
+              <polygon points="100.99 45.87 120.42 35.22 120.42 8.22 27.09 59.79 27.09 86.41 71.84 61.56 71.84 46.99 87.44 38.3 100.99 45.87"></polygon>
+              <polygon points="0 87.63 12.61 94.54 24.29 88 24.29 36.9 0 23.26 0 87.63"></polygon>
+              <polygon points="74.64 48.67 74.64 61.47 135.18 94.63 147.41 87.81 147.41 75.3 88.09 41.2 74.64 48.67"></polygon>
+              <polygon points="70.91 32.42 12.05 0 0 6.63 0 20.18 46.43 45.96 70.91 32.42"></polygon>
+              <polygon points="123.22 6.73 123.22 58.39 147.41 71.84 147.41 7.1 134.99 0.19 123.22 6.73"></polygon>
+              <path d="M177.78,14.27c0-3.41-2.27-5.9-6.61-5.9h-7V25.53h4.21V19.81H171l2.88,5.72h4.69L174.85,19A5.14,5.14,0,0,0,177.78,14.27Zm-6.61,2.06h-2.84v-4h2.84c1.49,0,2.36.7,2.36,1.93C173.53,15.79,172.39,16.33,171.17,16.33Z"></path>
+              <path d="M187.47,17.17a17,17,0,0,0-17-17h0a17,17,0,1,0,17,17Zm-16.9,14.09h-.12a14,14,0,1,1,.12,0Z"></path>
+            </g>
+          </svg>
+        </Link>
       </nav>
       <div className="flex justify-center py-10 items-center text-white">
         <ul className="flex items-start flex-col px-2 gap-3">
           {/* Home item */}
-          <Link to="/"  className={`font-semibold text-base ${
-              isActive("/") ? "bg-[#27272A] pr-20" : ""
+          <Link to="/" className={`font-semibold text-base ${isActive("/") ? "bg-[#27272A] pr-20" : ""
             } hover:bg-[#27272A] py-2 w-40 rounded-lg p-2`}>
             <li className="flex gap-1 items-center">
               <span className="mr-2">
@@ -80,20 +81,18 @@ function SideNavbar() {
 
 
           {/* Search item */}
-         <Link to="/search" className={`font-semibold text-sm ${
-              isActive("/search") ? "bg-[#27272A]" : ""
+          <Link to="/search" className={`font-semibold text-sm ${isActive("/search") ? "bg-[#27272A]" : ""
             } hover:bg-[#27272A] py-2 w-40 rounded-lg p-2`}>
-         <li className="flex gap-1 items-center">
-            <span className="mr-1.5">
-              <IoSearch className="text-2xl" />
-            </span>
-            <span className="font-semibold text-base">Search</span>
-          </li>
-         </Link>
+            <li className="flex gap-1 items-center">
+              <span className="mr-1.5">
+                <IoSearch className="text-2xl" />
+              </span>
+              <span className="font-semibold text-base">Search</span>
+            </li>
+          </Link>
 
           {/* Profile item */}
-          <Link to="profile" className={`font-semibold text-sm ${
-              isActive("/profile") ? "bg-[#27272A]" : ""
+          <Link to="profile" className={`font-semibold text-sm ${isActive("/profile") ? "bg-[#27272A]" : ""
             } hover:bg-[#27272A] py-2 w-40 rounded-lg p-2`}>
             <li className="flex gap-1 items-center">
               <span className="mr-1.5">
@@ -103,8 +102,7 @@ function SideNavbar() {
             </li>
           </Link>
 
-          <Link to="/premium" className={`font-semibold text-sm ${
-              isActive("/premium") ? "bg-[#27272A]" : ""
+          <Link to="/premium" className={`font-semibold text-sm ${isActive("/premium") ? "bg-[#27272A]" : ""
             } hover:bg-[#27272A] py-2 w-40 rounded-lg p-2`}>
             <li className="flex gap-2 items-center">
               <span className="mr-1">
@@ -131,25 +129,24 @@ function SideNavbar() {
             </li>
           </Link>
 
-        <Link to="/message" className={`font-semibold text-sm ${
-              isActive("/message") ? "bg-[#27272A]" : ""
+          <Link to="/message" className={`font-semibold text-sm ${isActive("/message") ? "bg-[#27272A]" : ""
             } hover:bg-[#27272A] py-2 w-40 rounded-lg p-2`}>
-        <li className="flex gap-1 items-center">
-            <span className="mr-2.5">
-              <svg
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-                fill="white"
-                className="w-5 h-5"
-              >
-                <g>
-                  <path d="M1.998 4.499c0-.828.671-1.499 1.5-1.499h17c.828 0 1.5.671 1.5 1.499v2.858l-10 4.545-10-4.547V4.499zm0 5.053V19.5c0 .828.671 1.5 1.5 1.5h17c.828 0 1.5-.672 1.5-1.5V9.554l-10 4.545-10-4.547z"></path>
-                </g>
-              </svg>
-            </span>
-            <span className="font-semibold text-base">Message</span>
-          </li>
-        </Link>
+            <li className="flex gap-1 items-center">
+              <span className="mr-2.5">
+                <svg
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                  fill="white"
+                  className="w-5 h-5"
+                >
+                  <g>
+                    <path d="M1.998 4.499c0-.828.671-1.499 1.5-1.499h17c.828 0 1.5.671 1.5 1.499v2.858l-10 4.545-10-4.547V4.499zm0 5.053V19.5c0 .828.671 1.5 1.5 1.5h17c.828 0 1.5-.672 1.5-1.5V9.554l-10 4.545-10-4.547z"></path>
+                  </g>
+                </svg>
+              </span>
+              <span className="font-semibold text-base">Message</span>
+            </li>
+          </Link>
 
 
           <li className="flex gap-3 ml-1 items-center">
@@ -162,9 +159,8 @@ function SideNavbar() {
                 <span className="font-semibold text-base">Logout</span>
               </button>
             ) : (
-              <Link to="/signin" className={`font-semibold text-sm ${
-                isActive("/signin") ? "bg-[#27272A]" : ""
-              } hover:bg-[#27272A] py-2 w-40 rounded-lg p-2`}>
+              <Link to="/signin" className={`font-semibold text-sm ${isActive("/signin") ? "bg-[#27272A]" : ""
+                } hover:bg-[#27272A] py-2 w-40 rounded-lg p-2`}>
                 <button className="flex items-center text-white gap-3">
                   <MdLogin className="text-2xl " />
                   <span className="font-semibold text-base">Login</span>
