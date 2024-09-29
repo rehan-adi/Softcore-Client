a415192c79792276285ac9a30c80540a6c4aecaf
Rehan <rehanalire52@gmail.com>
2024-09-29 23:28:42 +0530

    Fix Premium page imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Home from "../src/pages/Home";
+import { Toaster } from "react-hot-toast";
+import Premium from "../src/pages/Premium";
 import Post from "../src/components/Post";
 import Profile from "../src/components/Profile";
 import Search from "../src/components/Search";
-import Premium from "../src/components/Premium";
-import { Toaster } from "react-hot-toast";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
