8cbc89a8414de3ee92962081bf44468251acd5f3
Rehan <rehanalire52@gmail.com>
2024-09-29 23:25:31 +0530

    fix imports of Signup and Signin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
 import Home from "../src/pages/Home";
 import Post from "../src/components/Post";
 import Profile from "../src/components/Profile";
 import Search from "../src/components/Search";
 import Premium from "../src/components/Premium";
 import { Toaster } from "react-hot-toast";
-import Signup from "./components/auth/Signup";
-import Signin from "./components/auth/Signin";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
