1ceaa7a7197768f13ce689bb205f904bd0f94b1c
Rehan <rehanalire52@gmail.com>
2024-09-29 23:36:22 +0530

    Fix Search page import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Home from "../src/pages/Home";
+import Search from "../src/pages/Search";
 import { Toaster } from "react-hot-toast";
 import Premium from "../src/pages/Premium";
 import Post from "../src/components/Post";
 import Profile from "../src/components/Profile";
-import Search from "../src/components/Search";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
