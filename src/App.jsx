f2311f851617b9cb47fe37b06d57586541492174
Rehan <rehanalire52@gmail.com>
2024-09-30 11:34:49 +0530

    Fix import for Profile page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Home from "../src/pages/Home";
 import Search from "../src/pages/Search";
+import Post from "../src/components/Post";
 import { Toaster } from "react-hot-toast";
 import Premium from "../src/pages/Premium";
-import Post from "../src/components/Post";
-import Profile from "../src/components/Profile";
+import Profile from "../src/pages/Profile";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
