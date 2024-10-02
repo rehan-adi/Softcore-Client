bc8704a8cb3726d42aeb1a5992d7f1a1b3980cff
Rehan <rehanalire52@gmail.com>
2024-10-02 16:08:27 +0530

    Use comment model in Post component

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -9,6 +9,7 @@ import { MdOutlineEdit } from "react-icons/md";
 import { useGetPost } from "../hooks/useGetPost";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { FaRegCommentDots } from "react-icons/fa6";
+import CommentModal from "../components/CommentModal";
 import { useDeletePost } from "../hooks/usePostDelete";
 import { MdOutlineThumbUpOffAlt } from "react-icons/md";
 
@@ -26,6 +27,7 @@ function Post() {
   const [showCommentModal, setShowCommentModal] = useState(false);
   const [commentFormData, setCommentFormData] = useState({ content: "" });
   const [comments, setComments] = useState([]);
+
 
   const { loading, posts, error } = useGetPost();
   const { handleDelete, isDeleting } = useDeletePost(setBlogs, setSelectedBlog);
@@ -63,34 +65,6 @@ function Post() {
     }
   };
 
-
-  //   try {
-  //     const token = getToken();
-  //     await axios.delete(`http://localhost:3333/api/v1/blogs/delete/${postId}`, {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     });
-  //     setBlogs(blogs.filter((blog) => blog._id !== postId));
-  //     setSelectedBlog(null);
-  //     toast.success("Post deleted successfully!");
-  //   } catch (error) {
-  //     if (error.response) {
-  //       console.error("Error deleting post:", error.response.data.message);
-  //       toast.error(error.response.data.message);
-  //     } else if (error.request) {
-  //       console.error(
-  //         "Error deleting post: No response received",
-  //         error.request
-  //       );
-  //       toast.error("Error deleting post. Please try again.");
-  //     } else {
-  //       console.error("Error deleting post:", error.message);
-  //       toast.error("Error deleting post. Please try again.");
-  //     }
-  //   }
-  // };
-
   const handleEdit = (blog) => {
     setEditFormData({
       content: blog.content,
@@ -98,7 +72,6 @@ function Post() {
     setSelectedBlog(blog);
     setShowModal(true);
   };
-
 
   const closeModel = () => {
     setShowModal(false);
@@ -148,61 +121,9 @@ function Post() {
     }
   };
 
-
   const toggleCommentModal = () => {
-    setShowCommentModal(!showCommentModal);
-    if (!showCommentModal && selectedBlog && selectedBlog._id) {
-      fetchComments(selectedBlog._id);
-    }
-  };
-
-  const fetchComments = async (postId) => {
-    try {
-      const response = await axios.get(`http://localhost:3333/api/comments/${postId}`);
-      const { data } = response.data;
-      if (data && data.comments) {
-        setComments(data.comments);
-      } else {
-        throw new Error("No comments found in response data");
-      }
-    } catch (error) {
-      console.error("Error fetching comments:", error);
-      toast.error("Error fetching comments.");
-    }
-  };
-
-
-  const handleCommentChange = (e) => {
-    setCommentFormData({
-      ...commentFormData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-
-  const handleCommentSubmit = async (e) => {
-    e.preventDefault();
-    const token = getToken();
-    try {
-      const postId = selectedBlog._id;
-      const response = await axios.post(
-        `http://localhost:3333/api/v1/comments/post/${postId}/comments`,
-        commentFormData,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      const newComment = response.data.comment;
-      setComments([...comments, newComment]);
-      setCommentFormData({ content: "" });
-      toast.success("Comment added successfully!");
-    } catch (error) {
-      console.error("Error adding comment:", error);
-      toast.error("Error adding comment. Please try again.");
-    }
-  };
+    setShowCommentModal(prev => !prev);
+};
 
   return (
     <div className="flex relative z-10">
@@ -411,49 +332,15 @@ function Post() {
           </div>
         </div>
       )}
-      {showCommentModal && (
-        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-75">
-          <div className="bg-black border border-opacity-20 lg:w-[35vw] w-[90vw] h-[55vh] border-white p-6 rounded-lg">
-              <div className="flex justify-between mb-5 items-center">
-              <h2 className="text-xl font-semibold">Comments</h2>
-            <button
-                  type="button"
-                  onClick={toggleCommentModal}
-                  className=" text-white py-2 px-4 rounded-full ml-4 focus:outline-none focus:shadow-outline"
-                >
-                  < X />
-                </button>
-              </div>
-            <div className="overflow-y-auto max-h-[50vh]">
-              {comments.map((comment, index) => (
-                <div key={index} className="mb-4">
-                  <p className="text-white">{comment.content}</p>
-                  <p className="text-gray-400">{comment.author}</p>
-                </div>
-              ))}
-            </div>
-            <form onSubmit={handleCommentSubmit}>
-              <textarea
-                id="comment"
-                name="content"
-                value={commentFormData.content}
-                onChange={handleCommentChange}
-                className="appearance-none border border-white border-opacity-20 rounded bg-black w-full py-3 px-3 text-white placeholder:text-white leading-tight focus:outline-none h-44 focus:shadow-outline"
-                placeholder="Write a comment..."
-              />
-              <div className="flex items-center mt-7 justify-start">
-                <button
-                  type="submit"
-                  className="text-black bg-white font-bold py-2 px-4 rounded-full"
-                >
-                  Comment
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      )}
-
+      <CommentModal 
+      showCommentModal={showCommentModal}
+      toggleCommentModal={toggleCommentModal}
+      selectedBlog={selectedBlog}
+      comments={comments}
+      setComments={setComments}
+      commentFormData={commentFormData}
+      setCommentFormData={setCommentFormData}
+       />
     </div>
   );
 }
