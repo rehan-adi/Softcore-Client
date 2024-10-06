b24d4348951c0d5a9c7c08512841e6d9acdd912c
Rehan <rehanalire52@gmail.com>
2024-10-06 16:52:16 +0530

    Add Post store

---
+++ src/store/usePostsStore.js
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+
+export const usePostsStore = create((set) => ({
+  posts: [],
+  loading: false,
+
+  setPosts: (posts) => set(() => ({ posts })),
+  addPost: (post) => set((state) => ({ posts: [...state.posts, post] })),
+  updatePost: (postId, updatedPost) =>
+    set((state) => ({
+      posts: state.posts.map((post) =>
+        post._id === postId ? { ...post, ...updatedPost } : post
+      ),
+    })),
+  deletePost: (postId) =>
+    set((state) => ({
+      posts: state.posts.filter((post) => post._id !== postId),
+    })),
+  setLoading: (loading) => set(() => ({ loading })),
+}));
