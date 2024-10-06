87476b8fb72b1008ddad659967699afb896e2de9
Rehan <rehanalire52@gmail.com>
2024-10-06 17:20:24 +0530

    Add updatePost hook to code base

---
+++ src/hooks/useUpdatePost.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { useCallback } from 'react';
+import { toast } from 'react-hot-toast';
+import { getToken } from '../utils/token';
+import { BACKEND_API_URL } from '../constant';
+import { usePostsStore } from '../store/usePostsStore';
+
+export const useUpdatePost = () => {
+
+    const { updatePost, loading, setLoading } = usePostsStore();
+
+    const handleUpdatePost = useCallback(async (postId, updatedData) => {
+        setLoading(true);
+        const token = getToken();
+
+        try {
+            const response = await axios.patch(
+                `${BACKEND_API_URL}/blogs/update/${postId}`,
+                updatedData,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+
+            if (response.status === 200) {
+                updatePost(postId, data.updatedBlog);
+                toast.success("Post updated successfully!");
+            }
+
+        } catch (error) {
+            console.error("Error updating post:", error);
+            toast.error("Failed to update post. Please try again.");
+        } finally {
+            setLoading(false);
+        }
+    }, [updatePost, setLoading]);
+
+    return { handleUpdatePost, loading };
+};
