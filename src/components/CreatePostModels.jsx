e5d62e5bc98d1de910a9fc1b4a8258e28885bf61
Rehan <rehanalire52@gmail.com>
2024-10-07 12:29:41 +0530

    Update handleFormSubmit to reset form and close modal on successful post creation

--- src/components/CreatePostModels.jsx
+++ src/components/CreatePostModels.jsx
@@ -19,8 +19,11 @@ function CreatePostModal({ onClose }) {
   const { loading, onSubmitForm } = useCreatePost(); 
 
   const handleFormSubmit = (data) => {
-    onSubmitForm(data, onClose); 
-  };
+    onSubmitForm(data, () => {
+      reset(); 
+      onClose(); 
+    });
+};
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50">
