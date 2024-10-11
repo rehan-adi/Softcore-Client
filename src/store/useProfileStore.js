337c41ba34ce05050d7b24300f8e9668cd56d4b2
Rehan <rehanalire52@gmail.com>
2024-10-11 23:52:56 +0530

    Update on useProfileStore

--- src/store/useProfileStore.js
+++ src/store/useProfileStore.js
@@ -8,4 +8,14 @@ export const useProfileStore = create((set) => ({
     setProfileData: (profileData) => set(() => ({ profileData })),
     setPosts: (posts) => set(() => ({ posts })),
     setLoading: (loading) => set(() => ({ loading })),
+
+    deletePost: (postId) => set((state) => ({
+        posts: state.posts.filter((post) => post._id !== postId),
+    })),
+
+    updatePost: (updatedPost) => set((state) => ({
+        posts: state.posts.map((post) =>
+            post._id === updatedPost._id ? updatedPost : post
+        ),
+    })),
 }));
