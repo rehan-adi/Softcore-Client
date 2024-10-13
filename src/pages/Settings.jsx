b387f21c9704ae3c8b0c2d451f685deee05dce65
Rehan <rehanalire52@gmail.com>
2024-10-13 16:52:27 +0530

    Implement settings page with change password functionality and validation.

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,24 @@
+import { Loader } from 'lucide-react';
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useChangePassword } from '../hooks/useChangePassword';
+import { changePasswordValidation } from "../validations/auth.validation";
 
 const Settings = () => {
+
   const [activeSetting, setActiveSetting] = useState("changePassword");
+
+  const { handleChangePassword, loading } = useChangePassword();
+  const { register, handleSubmit, formState: { errors }, reset } = useForm({
+    resolver: zodResolver(changePasswordValidation),
+    mode: "onSubmit"
+  });
+
+  const onSubmit = (data) => {
+    handleChangePassword({ password: data.password });
+    reset()
+  };
 
   return (
     <div className="min-h-screen flex justify-start pt-5 md:px-40 items-center flex-col w-full bg-black text-white">
@@ -10,10 +27,9 @@ const Settings = () => {
           <li>
             <span
               className={`w-full p-2 cursor-pointer transition 
-                ${
-                  activeSetting === "changePassword"
-                    ? "md:border-b-4 border-b-2 border-white"
-                    : "md:hover:border-b-4 hover:border-b-2 hover:border-white"
+                ${activeSetting === "changePassword"
+                  ? "md:border-b-4 border-b-2 border-white"
+                  : "md:hover:border-b-4 hover:border-b-2 hover:border-white"
                 }
             `}
               onClick={() => setActiveSetting("changePassword")}
@@ -24,11 +40,10 @@ const Settings = () => {
           <li>
             <span
               className={`w-full p-2 cursor-pointer transition 
-            ${
-              activeSetting === "deleteAccount"
-                ? "md:border-b-4 border-b-2 border-white"
-                : "md:hover:border-b-4 hover:border-b-2  hover:border-white"
-            }
+            ${activeSetting === "deleteAccount"
+                  ? "md:border-b-4 border-b-2 border-white"
+                  : "md:hover:border-b-4 hover:border-b-2  hover:border-white"
+                }
              `}
               onClick={() => setActiveSetting("deleteAccount")}
             >
@@ -37,18 +52,29 @@ const Settings = () => {
           </li>
         </ul>
       </div>
-      <main className="flex flex-col items-center mt-10 justify-center">
+      <main className="flex flex-col px-5 items-center w-full md:w-[30vw] mt-10 justify-center">
         {activeSetting === "changePassword" && (
           <div className="p-5 rounded-lg shadow-md w-full max-w-lg">
             <h1 className="text-xl font-semibold mb-5">Change Password</h1>
-            <input
-              type="password"
-              placeholder="Enter New Password"
-              className="shadow appearance-none border rounded-full border-opacity-30 border-white bg-transparent w-full py-3 px-5 text-white leading-tight mb-3 focus:border-opacity-55 focus:outline-none focus:shadow-outline placeholder:text-white"
-            />
-            <button className="bg-white mt-5 w-44 font-semibold text-black px-4 py-2 rounded-full transition">
-              Update Password
-            </button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                type="password"
+                placeholder="Enter New Password"
+                {...register("password")}
+                disabled={loading}
+                className="shadow appearance-none border rounded-full border-opacity-30 border-white bg-transparent w-full py-3 px-5 text-white leading-tight mb-3 focus:border-opacity-55 focus:outline-none focus:shadow-outline placeholder:text-white"
+              />
+              {errors.password && (
+                <span className="text-red-500 inline-block">{errors.password.message}</span>
+              )}
+              <button
+                type="submit"
+                disabled={loading}
+                className="bg-white mt-5 w-44 font-semibold text-black px-4 py-2 rounded-full transition"
+              >
+                {loading ? <Loader className="w-6 h-6 animate-spin mr-3 inline-block" /> : "Update Password"}
+              </button>
+            </form>
           </div>
         )}
         {activeSetting === "deleteAccount" && (
