1674da5f0280cc2bc54497af679f5949580c6bea
Rehan <rehanalire52@gmail.com>
2024-10-16 13:24:23 +0530

    Add useComment hook for fetching and managing comments for a specific post.

--- src/hooks/useComment.js
+++ src/hooks/useComment.js
@@ -2,9 +2,37 @@ import axios from 'axios';
 import { toast } from "react-hot-toast";
 import { getToken } from "../utils/token";
 import { BACKEND_API_URL } from '../constant';
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
+export const useComment = (postId) => {
 
-export const useComment = () => {
+    const [comments, setComments] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const getComment = useCallback(async () => {
+        setLoading(true);
+        try {
+            const response = await axios.get(`${BACKEND_API_URL}/comments/${postId}`);
+
+            if (response.status == 200) {
+                setComments(response.data.comments);
+            }
+
+        } catch (error) {
+            console.error("Error fetching post's comments:", error);
+            const message = error.response?.data?.message || 'Error fetching comments';
+            toast.error(message);
+        } finally {
+            setLoading(false);
+        }
+    }, [postId]);
+
+    useEffect(() => {
+        if (postId) {
+            getComment();
+        }
+    }, [postId, getComment])
+
+    return { getComment, loading, comments };
 
 }
